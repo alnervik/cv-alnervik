a3fd5996a9ccc4c74f14186a523d88f415a52e80
alnervik <k.alnervik@gmail.com>
2024-09-20 10:34:43 +0200

    added so that change from dark to light mode also swaps the logo

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 // Funktion för att växla mellan ljust och mörkt läge
 function toggleTheme() {
     const body = document.body;
+    const themeIcon = document.querySelector('.light-mode i');
+    
     body.classList.toggle('dark-mode');
     
-    // Sparar användarens val i localStorage
+    // Ändra ikonen när användare byter färg-läge
     if (body.classList.contains('dark-mode')) {
+        themeIcon.classList.remove('fa-moon');
+        themeIcon.classList.add('fa-sun');
         localStorage.setItem('theme', 'dark');
     } else {
+        themeIcon.classList.remove('fa-sun');
+        themeIcon.classList.add('fa-moon');
         localStorage.setItem('theme', 'light');
     }
 }
@@ -18,7 +24,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Kontrollera användarens sparade inställning
     const savedTheme = localStorage.getItem('theme');
+    const themeIcon = document.querySelector('.light-mode i');
+    
     if (savedTheme === 'dark') {
         document.body.classList.add('dark-mode');
+        themeIcon.classList.remove('fa-moon');
+        themeIcon.classList.add('fa-sun');
     }
 });
